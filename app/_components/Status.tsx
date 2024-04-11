91c07f20934e7234925c01f6d2d902614e08604c
Pierre Peinado <pierre.peinado@webinage.fr>
2024-04-11 14:38:12 +0200

    Contact me section

--- app/_components/Status.tsx
+++ app/_components/Status.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
-import { Camera, Gift, Grid3X3, LucideIcon } from "lucide-react";
+import { ArrowUpRight, Camera, Gift, Grid3X3, LucideIcon } from "lucide-react";
 import Link from "next/link";
 import { Section } from "./Section";
 
@@ -28,8 +28,26 @@ export const Status = () => {
             ))}
           </div>
         </Card>
-        <Card className="flex-1 p-4">
+        <Card className="flex-1 p-4 flex flex-col gap-2">
           <p className="text-lg text-muted-foreground">Contact me</p>
+          <ContactCard
+            name="Email"
+            image="images/pierre.png"
+            mediumImage="https://upload.wikimedia.org/wikipedia/fr/a/a7/Mail_%28Apple%29_logo.png"
+            description="ppeinadop@gmail.com"
+          />
+          <ContactCard
+            name="Phone"
+            image="images/pierre.png"
+            mediumImage="images/phone.png"
+            description="+33 6 62 88 10 24"
+          />
+          <ContactCard
+            name="LinkedIn"
+            image="images/pierre.png"
+            mediumImage="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+            description="Pierre Peinado"
+          />
         </Card>
       </div>
     </Section>
@@ -139,5 +157,41 @@ const Work = (props: WorkProps) => {
       </div>
       <p className="text-xs text-end text-muted-foreground">{props.date}</p>
     </Link>
+  );
+};
+
+const ContactCard = (props: {
+  image: string;
+  mediumImage: string;
+  name: string;
+  description: string;
+}) => {
+  return (
+    <Card className="p-3 bg-accent/10 hover:bg-accent/30 transition-colors flex items-center gap-4 group">
+      <div className="relative">
+        <img
+          src={props.image}
+          alt={props.name}
+          className="w-10 h-10 rounded-full object-container"
+        />
+        <img
+          src={props.mediumImage}
+          alt={props.name}
+          className="w-4 h-4 absolute -bottom-1 -right-1 rounded-full object-container"
+        />
+      </div>
+      <div className="mr-auto">
+        <div className="flex items-center gap-2">
+          {/* https://ui.shadcn.com/docs/components/typography#large */}
+          <p className="text-lg font-semibold">{props.name}</p>
+        </div>
+        {/* https://ui.shadcn.com/docs/components/typography#muted */}
+        <p className="text-xs text-muted-foreground">{props.description}</p>
+      </div>
+      <ArrowUpRight
+        size={16}
+        className="mr-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform"
+      />
+    </Card>
   );
 };
